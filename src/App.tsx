accb9b6d39a5ba20a557bee0111c6a97a7d1b97e
Zach Tullis <zachjtullis@gmail.com>
2024-03-07 16:06:25 -0700

    Got the Teams to show up!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,64 @@
+import React from 'react';
+import teamsData from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function App() {
+  const teams = teamsData.teams;
+
+  function Welcome() {
+    return <h1>College Basketball Teams</h1>;
+  }
+
+  class Team extends React.Component<TeamProps> {
+    render() {
+      const oneTeam = this.props;
+
+      return (
+        <div>
+          <h2>{oneTeam.school}</h2>
+          <h2>Mascot: {oneTeam.name}</h2>
+          <h2>
+            Location: {oneTeam.city}, {oneTeam.state}
+          </h2>
+        </div>
+      );
+    }
+  }
+
+  function TeamList() {
+    return (
+      <div>
+        {teams.map((teamNum) => (
+          <Team {...teamNum} />
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <Welcome />
+      <TeamList />
+    </div>
+  );
+}
+
+export default App;
+
+/*
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
@@ -67,3 +128,4 @@ function App() {
 }
 
 export default App;
+*/
