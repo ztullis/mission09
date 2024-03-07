4799d851b4e30c1a997cb01a2d9625326c232edc
Zach Tullis <zachjtullis@gmail.com>
2024-03-07 16:16:05 -0700

    FINSHED!!!

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,17 @@ function App() {
   const teams = teamsData.teams;
 
   function Welcome() {
-    return <h1>College Basketball Teams</h1>;
+    return (
+      <h1
+        style={{
+          border: '2px solid black',
+          textAlign: 'center',
+          padding: '20px',
+        }}
+      >
+        <b>College Basketball Teams</b>
+      </h1>
+    );
   }
 
   class Team extends React.Component<TeamProps> {
@@ -27,12 +37,21 @@ function App() {
       const oneTeam = this.props;
 
       return (
-        <div>
-          <h2>{oneTeam.school}</h2>
-          <h2>Mascot: {oneTeam.name}</h2>
-          <h2>
+        <div
+          style={{
+            border: '2px solid black',
+            textAlign: 'center',
+            padding: '20px',
+          }}
+        >
+          <b>
+            <h2>{oneTeam.school}</h2>
+          </b>
+          <p>Mascot: {oneTeam.name}</p>
+          <p>
             Location: {oneTeam.city}, {oneTeam.state}
-          </h2>
+          </p>
+          <br></br>
         </div>
       );
     }
@@ -57,75 +76,3 @@ function App() {
 }
 
 export default App;
-
-/*
-import React, { useState, useEffect } from 'react';
-import './App.css';
-
-interface TeamProps {
-  tid: number;
-  cid: number;
-  did: number;
-  school: string;
-  name: string;
-  abbrev: string;
-  pop: number;
-  city: string;
-  state: string;
-  latitude: number;
-  longitude: number;
-}
-
-function App() {
-  const [teamNames, setTeamNames] = useState<TeamProps[]>([]);
-
-  useEffect(() => {
-    fetch('./CollegeBasketballTeams.json')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setTeamNames(data.teams);
-      })
-      .catch((error) => console.error('Error fetching team data:', error));
-  }, []);
-
-  function Welcome() {
-    return <h1>College Basketball Teams</h1>;
-  }
-
-  class Team extends React.Component<TeamProps> {
-    render() {
-      const oneTeam = this.props;
-
-      return (
-        <div>
-          <h2>{oneTeam.school}</h2>
-          <h2>Mascot: {oneTeam.name}</h2>
-          <h2>
-            Location: {oneTeam.city}, {oneTeam.state}
-          </h2>
-        </div>
-      );
-    }
-  }
-
-  function TeamList() {
-    return (
-      <div>
-        {teamNames.map((teamNum: TeamProps, index: number) => (
-          <Team key={index} {...teamNum} />
-        ))}
-      </div>
-    );
-  }
-
-  return (
-    <div className="App">
-      <Welcome />
-      <TeamList />
-    </div>
-  );
-}
-
-export default App;
-*/
